29e3223aa9ccfe57cec17bdcd073dc6f2361de4a
mahojv <moisesmjavier@outlook.com>
2025-04-19 21:44:37 -0600

    ajustes para mejora de accesibilidad y seo 1

--- src/App.jsx
+++ src/App.jsx
@@ -42,12 +42,12 @@ export default function App() {
     <div >
 
       <div className='z-0 w-full h-[200px]  flex flex-col items-center relative'>
-        <div role='header' className='w-full'>
-          <figure role='imagen encabezado' className=' h-[240px] absolute w-full overflow-hidden' onClick={closeModal}>
+        <div  className='w-full'>
+          <figure  className=' h-[240px] absolute w-full overflow-hidden' onClick={closeModal}>
             <img className='object-cover  h-full w-full' src="./resources/hero-image-github-profile.jpg" alt="" />
           </figure>
         </div>
-        <div className=' z-40 min-w-[360px]  lg:w-[490px] h-[57px] rounded-lg bg-fondoGrisColor flex items-center gap-4 mt-[32px] px-4  ' role='search'>
+        <div className=' z-40 min-w-[360px]  lg:w-[490px] h-[57px] rounded-lg bg-fondoGrisColor flex items-center gap-4 mt-[32px] px-4  ' >
           <figure><img src="./resources/Search.svg" alt="" /></figure>
           <input className='placeholder:text-cardTextColor w-full h-full text-cardTextColor' type="text" placeholder='Username' onKeyDown={handleKeyDown} onClick={toggleFunction} onChange={quickSearchFunction} />
         </div>
@@ -66,12 +66,12 @@ export default function App() {
         <div className=' flex flex-col lg:flex-row lg:items-center  gap-[24px]'>
 
           <div className=' z-50 w-[120px] h-[120px] rounded-xl bg-fondoGrisColor flex justify-center items-center'>
-            <figure role='profile img ' className='w-[105px] overflow-hidden rounded-xl'>
+            <figure  className='w-[105px] overflow-hidden rounded-xl'>
               <img src={response?.avatar_url} alt="" className='object-cover ' />
             </figure>
           </div>
 
-          <div role='infContainer' className='flex flex-wrap gap-[22px] lg:mt-9 text-sm font-medium '>
+          <div  className='flex flex-wrap gap-[22px] lg:mt-9 text-sm font-medium '>
 
             <div className='bg-fondoCardBodyColor text-cardTextColor  flex  gap-3 w-fit h-[53px] items-center justify-center rounded-xl  '>
               <div className='border-r  border-bodyTextColor/50   text-center flex justify-center  px-[23px] h-[70%] items-center '>
